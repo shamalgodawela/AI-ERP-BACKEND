afa48a96abd4497628a45c79d68fa93763a28092
shamal melantha <shamalmelantha3@gmail.com>
2024-03-07 10:41:28 +0530

    update

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -221,16 +221,6 @@ const getLastInvoiceNumber = async (req, res) => {
         res.status(500).json({ success: false, message: 'Failed to fetch last numbers', error: error.message });
     }
 };
-const checkInvoice = async (orderNumber) => {
-  try {
-    // Check if the invoice number exists in the database
-    const existingInvoice = await Invoice.findOne({ orderNumber });
-    return !!existingInvoice; // Convert the result to a boolean
-  } catch (error) {
-    console.error('Error checking invoice:', error);
-    return false;
-  }
-};
 
 
 
@@ -244,7 +234,7 @@ module.exports = {
   getTotalInvoiceValueByCode,
   getMonthlyTotalInvoice,
   getLastInvoiceNumber,
-  checkInvoice
+  
 };
 
 
