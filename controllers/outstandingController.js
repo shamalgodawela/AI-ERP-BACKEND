bca8a9240bd55cf423932849e008d0505b3a4a01
shamal melantha <shamalmelantha3@gmail.com>
2024-03-12 11:54:34 +0530

    update outstanding

--- controllers/outstandingController.js
+++ controllers/outstandingController.js
@@ -80,17 +80,13 @@ const outstandingController = {
     searchOutstanding: async (req, res) => {
         try {
             // Extract search parameters from the request query
-            const { exe, status } = req.query;
+            const { exe } = req.query;
     
             // Build the search query based on the provided parameters
             const searchQuery = {};
     
             if (exe) {
                 searchQuery.exe = exe;
-            }
-    
-            if (status) {
-                searchQuery.status = status; // Add status to the search query
             }
     
             // Perform the search using your model
@@ -104,7 +100,6 @@ const outstandingController = {
             res.status(500).json({ error: 'Internal server error' });
         }
     }
-    
     
     
     
