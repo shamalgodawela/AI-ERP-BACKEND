cf5931d008b994bf647c6cdeb39397b47b300574
shamal melantha <shamalmelantha3@gmail.com>
2024-03-14 13:48:15 +0530

    update dateproduct

--- controllers/dateproductController.js
+++ controllers/dateproductController.js
@@ -21,6 +21,20 @@ const addProductAndUpdate = async (req, res) => {
             existingProduct.quantity = parsedExistingQuantity + parsedNumberOfUnits;
             await existingProduct.save();
 
+            // Find bulk product with the same product code
+            const bulkProduct = await BulkProduct.findOne({ 'products.productCode': category });
+
+            if (bulkProduct) {
+                // Reduce the quantity in the BulkProduct
+                const productIndex = bulkProduct.products.findIndex(product => product.productCode === category);
+                bulkProduct.products[productIndex].quantity -= parsedNumberOfUnits;
+                await bulkProduct.save();
+            } else {
+                console.log('No bulkProduct found with the same product code.');
+                // If no bulk product found, return error
+                return res.status(404).json({ success: false, message: 'No bulkProduct found with the same product code.' });
+            }
+
             // Create a new product using the dateProduct data
             const newProduct = await DateProduct.create({
                 GpnDate,
