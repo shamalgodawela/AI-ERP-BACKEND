88f281c6c18afb5b04fb6bc0298c94ee622db988
shamal melantha <shamalmelantha3@gmail.com>
2024-03-19 15:07:02 +0530

    update dateproduct

--- controllers/dateproductController.js
+++ controllers/dateproductController.js
@@ -49,7 +49,77 @@ const addProductAndUpdate = async (req, res) => {
                     bulkProductGRN25.quantity = newQuantityGRN25;
                     await bulkProductGRN25.save();
                 }
-            } else {
+            } 
+            else if (category === 'MNP10') {
+                // Find the bulk code MNPB50 in bulk products
+                let bulkProductMNPB50 = await BulkProduct.findOne({ bulkCode: 'MNPB50' });
+                if (bulkProductMNPB50) {
+                    // Calculate newQuantity for bulk code BPB15
+                    let newQuantityMNPB50 = (bulkProductMNPB50.weight * bulkProductMNPB50.quantity - numberOfUnits * 6) / bulkProductMNPB50.weight;
+
+                    // Update quantity for bulk code BPB15
+                    bulkProductMNPB50.quantity = newQuantityMNPB50;
+                    await bulkProductMNPB50.save();
+                }
+
+                // Find the bulk code GRN25 in bulk products
+                let bulkProductGRN25 = await BulkProduct.findOne({ bulkCode: 'GRN25' });
+                if (bulkProductGRN25) {
+                    // Calculate newQuantity for bulk code GRN25
+                    let newQuantityGRN25 = (bulkProductGRN25.weight * bulkProductGRN25.quantity - numberOfUnits * 4) / bulkProductGRN25.weight;
+
+                    // Update quantity for bulk code GRN25
+                    bulkProductGRN25.quantity = newQuantityGRN25;
+                    await bulkProductGRN25.save();
+                }
+            } 
+            else if (category === 'MG800') {
+                // Find the bulk code MNPB50 in bulk products
+                let bulkProductMGB25 = await BulkProduct.findOne({ bulkCode: 'MGB25' });
+                if (bulkProductMGB25) {
+                    // Calculate newQuantity for bulk code BPB15
+                    let newQuantityMGB25 = (bulkProductMGB25.weight * bulkProductMGB25.quantity - numberOfUnits * parsedPacksize) / bulkProductMGB25.weight;
+
+                    // Update quantity for bulk code BPB15
+                    bulkProductMGB25.quantity = newQuantityMGB25;
+                    await bulkProductMGB25.save();
+                }
+
+                // Find the bulk code WSB1000 in bulk products
+                let bulkProductWSB1000 = await BulkProduct.findOne({ bulkCode: 'WSB1000' });
+                if (bulkProductWSB1000) {
+                    // Calculate newQuantity for bulk code GRN25
+                    let newQuantityWSB1000 = (bulkProductWSB1000.weight * bulkProductWSB1000.quantity - numberOfUnits * 20) / bulkProductWSB1000.weight;
+
+                    // Update quantity for bulk code GRN25
+                    bulkProductWSB1000.quantity = newQuantityWSB1000;
+                    await bulkProductWSB1000.save();
+                }
+            } 
+            else if (category === 'MG400') {
+                // Find the bulk code MNPB50 in bulk products
+                let bulkProductMGB25 = await BulkProduct.findOne({ bulkCode: 'MGB25' });
+                if (bulkProductMGB25) {
+                    // Calculate newQuantity for bulk code BPB15
+                    let newQuantityMGB25 = (bulkProductMGB25.weight * bulkProductMGB25.quantity - numberOfUnits * parsedPacksize) / bulkProductMGB25.weight;
+
+                    // Update quantity for bulk code BPB15
+                    bulkProductMGB25.quantity = newQuantityMGB25;
+                    await bulkProductMGB25.save();
+                }
+
+                // Find the bulk code WSB1000 in bulk products
+                let bulkProductWSB1000 = await BulkProduct.findOne({ bulkCode: 'WSB1000' });
+                if (bulkProductWSB1000) {
+                    // Calculate newQuantity for bulk code GRN25
+                    let newQuantityWSB1000 = (bulkProductWSB1000.weight * bulkProductWSB1000.quantity - numberOfUnits * 40) / bulkProductWSB1000.weight;
+
+                    // Update quantity for bulk code GRN25
+                    bulkProductWSB1000.quantity = newQuantityWSB1000;
+                    await bulkProductWSB1000.save();
+                }
+            } 
+            else {
                 // Find bulk products with the same product code
                 const bulkProducts = await BulkProduct.find({ 'products.productCode': category });
 
@@ -67,6 +137,8 @@ const addProductAndUpdate = async (req, res) => {
                     return res.status(404).json({ success: false, message: 'No bulkProduct found with the same product code.' });
                 }
             }
+            
+            
 
             const totweight = numberOfUnits * parseInt(packsize);
 
