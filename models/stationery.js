7880688a1f0e423b88d53b28ebf902fdf995d6a2
shamal melantha <shamalmelantha3@gmail.com>
2024-04-01 13:14:15 +0530

    update stationery model

--- models/stationery.js
+++ models/stationery.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     code: {
         type: String,
-        required: true
+        required: true,
+        unique:true
     },
     name: {
         type: String,
