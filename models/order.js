9ddfa53cfc664c0d5a3009686fd8db939c46f814
shamal melantha <shamalmelantha3@gmail.com>
2024-04-25 10:11:19 +0530

    update order

--- models/order.js
+++ models/order.js
@@ -9,7 +9,7 @@ const orderSchema = new mongoose.Schema({
       labelPrice: Number,
        discount: {
         type: String,
-        
+        default: '...'
     },
       unitPrice: Number,
       invoiceTotal: Number,
