9f5b1037dbb15aa665660d0d16812e62e0a24d83
shamal melantha <shamalmelantha3@gmail.com>
2024-05-28 11:05:19 +0530

    update invoice rouute

--- routes/invoiceRoutes.js
+++ routes/invoiceRoutes.js
@@ -10,7 +10,7 @@ router.get('/get-total-invoice-value/:code', invoiceController.getTotalInvoiceVa
 router.get('/monthly-total-invoice/:code', invoiceController.getMonthlyTotalInvoice);
 router.get('/lastInvoiceNumber',invoiceController.getLastInvoiceNumber);
 router.get('/check/:orderNumber', invoiceController.checkOrderNumberExists);
-// Route for searching invoices
+
 router.get('/search-invoices', invoiceController.searchInvoices);
 router.put('/invoices/:invoiceNumber', invoiceController.updateInvoice);
 router.get('/invoices/:invoiceNumber', invoiceController.getInvoiceByNumber);
