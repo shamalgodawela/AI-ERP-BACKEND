7b3cdad9298e9be164cec6e344c0860989eff0b7
shamal melantha <shamalmelantha3@gmail.com>
2026-07-30 15:26:00 +0530

    Add authorization middleware to all order routes for enhanced security

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -6,31 +6,31 @@ const authorize = require("../middleWare/authorize");
 const router = express.Router();
 const ordersController = new OrdersController();
 
-router.use(protect, authorize("user","admin","Operation","account"));
-
-router.post('/orders', ordersController.addOrder);
 
 
-router.get('/allorders', ordersController.getAllOrders);
+router.post('/orders',protect, authorize("user","admin","Operation","account"), ordersController.addOrder);
 
 
-router.get('/orders/:orderNumber', ordersController.getOrderDetails);
+router.get('/allorders', protect, authorize("user","admin","Operation","account"), ordersController.getAllOrders);
 
 
-router.put('/orders/:orderNumber', ordersController.updateOrderDetails);
-
-router.get('/allor', ordersController.getAllOr);
+router.get('/orders/:orderNumber', protect, authorize("user","admin","Operation","account"), ordersController.getOrderDetails);
 
 
-router.get('/lastorder/ea', ordersController.getLastOrderNumberStartingWithEA);
-router.get('/lastorder/KU1', ordersController.getLastOrderNumberStartingWithKU1);
-router.get('/lastorder/NCP', ordersController.getLastOrderNumberStartingWithNCP);
-router.get('/lastorder/NUM', ordersController.getLastOrderNumberStartingWithNUM)
-router.get('/lastorder/PT1', ordersController.getLastOrderNumberStartingWithPT1)
-router.get('/lastorder/south1', ordersController.getLastOrderNumberStartingWithSouth1);
-router.get('/lastorder/upcountry', ordersController.getLastOrderNumberStartingWithUpccountry);
-router.get('/lastorder/other', ordersController.getLastOrderNumberStartingWithother);
-router.get('/lastorder/UpCountry1', ordersController.getLastOrderNumberStartingWithUpccountry1);
+router.put('/orders/:orderNumber', protect, authorize("user","admin","Operation","account"), ordersController.updateOrderDetails);
+
+router.get('/allor', protect, authorize("user","admin","Operation","account"), ordersController.getAllOr);
+
+
+router.get('/lastorder/ea', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithEA);
+router.get('/lastorder/KU1', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithKU1);
+router.get('/lastorder/NCP', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithNCP);
+router.get('/lastorder/NUM', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithNUM)
+router.get('/lastorder/PT1', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithPT1)
+router.get('/lastorder/south1', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithSouth1);
+router.get('/lastorder/upcountry', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithUpccountry);
+router.get('/lastorder/other', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithother);
+router.get('/lastorder/UpCountry1', protect, authorize("user","admin","Operation","account"), ordersController.getLastOrderNumberStartingWithUpccountry1);
 
 
 
