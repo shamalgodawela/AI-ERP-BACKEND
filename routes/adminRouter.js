856c5de69b70055f3afd02a417a251841d30b029
shamal melantha <shamalmelantha3@gmail.com>
2026-08-03 12:13:47 +0530

    Add getAllUsers endpoint and refactor adminController imports in adminRouter

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/adminController');
+const adminController = require('../controllers/adminController');
 const { loginUser } = require('../controllers/authController');
 const protect = require("../middleWare/authMiddleware");
 const authorize = require("../middleWare/authorize");
 
 
-router.post('/register/admin', protect,authorize("admin"), registerUser);
+router.post('/register/admin', protect,authorize("admin"), adminController.registerUser);
 router.post('/login', protect,authorize("admin"), loginUser);
+router.get('/get-all-users', protect, authorize("admin"), adminController.getAllUsers);
 
 module.exports = router;
